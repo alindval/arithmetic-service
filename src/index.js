0125456f247cdc272b84fdbecbc8bb49e0ca774a
Abigail Lindvall <abbyl1992@gmail.com>
2024-02-09 18:56:59 -0500

    updated res.send

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 app.use(cors());
 const port = 3000;
-app.get('/',(req,res)=>{
-    res.send("no request")
+app.get('/', (req, res) => {
+    res.send("Arithmetic Service Hello World")
 
 });
 
-app.get('/add/:n/:m',(req,res)=>{
-    res.json(Number(req.params.n)+Number(req.params.m))
+app.get('/add/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) + Number(req.params.m))
 
 });
 
